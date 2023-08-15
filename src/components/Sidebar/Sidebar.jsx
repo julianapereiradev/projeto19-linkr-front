b8b9ea295495562e9ea32af5bb3a325c6e10a54d
andre <demaiam@gmail.com>
2023-08-14 23:49:58 -0300

    started sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { SideBarContainer, Title, Line, Hashtag } from "./Styles";
+
+export default function App() {
+  const [hashtags, setHashtags] = useState([
+    "javascript", "react", "react-native", "material", "web-dev", "mobile", "css", "html", "node", "sql"
+  ]);
+
+  return (
+    <SideBarContainer>
+      <Title> trending </Title>
+      <Line />
+      {hashtags.map((hashtag, index) =>
+        <Hashtag key={index}>
+          # {hashtag}
+        </Hashtag>
+      )}
+    </SideBarContainer>
+  );
+}
---
+++ src/components/Sidebar/Styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const SideBarContainer = styled.aside`
+  display: flex;
+  flex-direction: column;
+  box-sizing: content-box;
+  background-color: #171717;
+  border-radius: 15px;
+  padding: 1px 15px 10px;
+  width: 20%;
+`;
+
+export const Title = styled.h1`
+  font-family: 'Oswald', sans-serif;
+  font-weight: 700;
+  font-size: 20px;
+  color: #FFFFFF;
+`;
+
+export const Line = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: #484848;
+  margin-bottom: 5px;
+  margin-top: 1px;
+`;
+
+export const Hashtag = styled.h2`
+  font-family: 'Lato', sans-serif;
+  font-weight: 700;
+  font-size: 15px;
+  color: #FFFFFF;
+`;
