85d45cb243874210c6fc3eed4b6621697c36b56d
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-19 20:24:59 -0300

    Update: post box edit post

--- src/components/PostBox/PostBox.jsx
+++ src/components/PostBox/PostBox.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import { styled } from "styled-components";
 import AuthContext from "../../contexts/AuthContext";
 import axios from "axios";
@@ -13,19 +13,22 @@ import NoImage from "../../assets/noimage2.png";
 import { TbTrashFilled } from "react-icons/tb";
 import Modal from "react-modal";
 import { ThreeDots } from "react-loader-spinner";
-
+import Pen from "../../assets/pen.png";
 
 export default function PostBox({ post }) {
   const { user } = useContext(AuthContext);
 
-    const [selected, setSelected] = useState(false);
-    const [urlMetadataInfo, setUrlMetadataInfo] = useState(null);
-    const [postLikes, setPostLikes] = useState();
-    const [showDeleteModal, setShowDeleteModal] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+  const [selected, setSelected] = useState(false);
+  const [urlMetadataInfo, setUrlMetadataInfo] = useState(null);
+  const [postLikes, setPostLikes] = useState();
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(post.content);
+  const textInputRef = useRef(null);
+  const [changesMade, setChangesMade] = useState(false);
 
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   function openUrlId(userId) {
     navigate(pages.userPosts + userId);
@@ -42,49 +45,94 @@ export default function PostBox({ post }) {
     }
   };
 
-
-    useEffect(() => {
-        const promiseLikes = axios.get(backendroute.getlikes + post.id, headersAuth(user.token))
-        promiseLikes.then((res) => {
-            setPostLikes(res.data)
-        })
-
-        if (post.url) {
-            fetchUrlMetadata(post.url);
-          }
-    }, [selected, post.id, post.url, user])
-
-    if (!postLikes) {
-        return <Load><ThreeDots color="#FFFFFF" height={50} width={50} /></Load>
+  const startEditing = () => {
+    setIsEditing(true);
+    if (textInputRef.current) {
+      textInputRef.current.focus();
     }
-
-    function like() {
-
-        const promiseUser = axios.get(
-            backendroute.getDataUserByToken,
-            headersAuth(user.token)
-        );
-        promiseUser.then((res) => {
-            const body = {
-                userId: res.data[0].userId,
-                postId: post.id,
-            };
-            const promise = axios.post(
-                backendroute.likes,
-                body,
-                headersAuth(user.token)
-            );
-            promise.then((res) => {
-                setSelected(!selected);
-            });
-            promise.catch((err) => {
-                alert(err.response.data);
-            });
-        });
-        promiseUser.catch((err) => {
-            alert(err.response.data);
-        });
+  };
+  
+  const handleCancelEdit = () => {
+    if (changesMade) {
+      const confirmDiscard = window.confirm("Descartar alterações não salvas?");
+      if (confirmDiscard) {
+        setIsEditing(false);
+        setChangesMade(false);
+        setEditedContent(post.content); 
+      }
+    } else {
+      setIsEditing(false);
     }
+  };
+
+  const handleSaveEdit = async (postId) => {
+    axios
+      .put(backendroute.updatePostById + postId, { content: editedContent }, headersAuth(user.token))
+      .then((response) => {
+        setIsEditing(false);
+        setChangesMade(false);
+        setEditedContent(response.data.content);
+      })
+      .catch((error) => {
+        console.error("Erro ao atualizar o post:", error);
+      });
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); 
+      handleSaveEdit(); 
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") {
+      handleCancelEdit();
+    }
+  };
+
+  useEffect(() => {
+    const promiseLikes = axios.get(backendroute.getlikes + post.id, headersAuth(user.token))
+    promiseLikes.then((res) => {
+      setPostLikes(res.data)
+    })
+
+    if (post.url) {
+      fetchUrlMetadata(post.url);
+    }
+  }, [selected, post.id, post.url, user])
+
+  if (!postLikes) {
+    return <Load><ThreeDots color="#FFFFFF" height={50} width={50} /></Load>
+  }
+
+  function like() {
+
+    const promiseUser = axios.get(
+      backendroute.getDataUserByToken,
+      headersAuth(user.token)
+    );
+    promiseUser.then((res) => {
+      const body = {
+        userId: res.data[0].userId,
+        postId: post.id,
+      };
+      const promise = axios.post(
+        backendroute.likes,
+        body,
+        headersAuth(user.token)
+      );
+      promise.then((res) => {
+        setSelected(!selected);
+      });
+      promise.catch((err) => {
+        alert(err.response.data);
+      });
+    });
+    promiseUser.catch((err) => {
+      alert(err.response.data);
+    });
+  }
 
   const openDeleteModal = () => {
     setShowDeleteModal(true);
@@ -110,7 +158,7 @@ export default function PostBox({ post }) {
     removeItem(postId);
   };
 
- 
+
 
   return (
     <>
@@ -123,21 +171,21 @@ export default function PostBox({ post }) {
               alt="Usuário"
             />
           </ContainerPhoto>
-           <Icon>
-                        <LikeTooltip >
-                            {postLikes[0].isLiked ? (
-                                <FaHeart color="#AC0000" size={20} onClick={() => like()} />
-                            ) : (
-                                <FiHeart color="#fff" size={20} onClick={() => like()} />
-                            )}
-                            <a data-tooltip-id={String(post.id)} data-tooltip-place="bottom">
-                                <SCQntLikes>{postLikes[0].count} likes</SCQntLikes>
-                            </a>
-                            <SCTooltip id={String(post.id)} style={{ backgroundColor: "#fff" }}>
-                                <SCTooltipText>{postLikes[0].whoLiked}</SCTooltipText>
-                            </SCTooltip>
-                        </LikeTooltip>
-                    </Icon>
+          <Icon>
+            <LikeTooltip >
+              {postLikes[0].isLiked ? (
+                <FaHeart color="#AC0000" size={20} onClick={() => like()} />
+              ) : (
+                <FiHeart color="#fff" size={20} onClick={() => like()} />
+              )}
+              <a data-tooltip-id={String(post.id)} data-tooltip-place="bottom">
+                <SCQntLikes>{postLikes[0].count} likes</SCQntLikes>
+              </a>
+              <SCTooltip id={String(post.id)} style={{ backgroundColor: "#fff" }}>
+                <SCTooltipText>{postLikes[0].whoLiked}</SCTooltipText>
+              </SCTooltip>
+            </LikeTooltip>
+          </Icon>
         </ContainerLike>
 
         <ContainerContent>
@@ -145,11 +193,13 @@ export default function PostBox({ post }) {
             <Username onClick={() => openUrlId(post.userId)}>
               {post.username}
             </Username>
-            <ButtonTrash data-test="delete-btn" onClick={openDeleteModal}><TbTrashFilled
-              color="#FFFFFF"
-              size="25"
-            /></ButtonTrash>
-            
+            <PenTrashContainer>
+              <PenContainer src={Pen} alt="pen" onClick={isEditing ? handleCancelEdit : startEditing}/>
+              <ButtonTrash data-test="delete-btn" onClick={openDeleteModal}><TbTrashFilled
+                color="#FFFFFF"
+                size="25"
+              /></ButtonTrash>
+            </PenTrashContainer>
           </ContainerTrashRow>
 
           <Modal
@@ -179,40 +229,55 @@ export default function PostBox({ post }) {
             }}
           >
             <DivInsideModal>
-            <h2 style={{textAlign: 'center'}}>Are you sure you want to delete this post?</h2>
-            <ButtonsModal>
-              <button data-test="cancel" onClick={() => setShowDeleteModal(false)}>
-                No, go back
-              </button>
-              <ButtonYes
-                data-test="confirm"
-                onClick={() => trashIconClicked(post.id)}
-                disabled={isLoading}
-              >
-                {isLoading ? (
-                  <ThreeDots
-                    type="ThreeDots"
-                    color="#000000"
-                    height={20}
-                    width={50}
-                  />
-                ) : (
-                  "Yes, delete it"
-                )}
-              </ButtonYes>
-            </ButtonsModal>
+              <h2 style={{ textAlign: 'center' }}>Are you sure you want to delete this post?</h2>
+              <ButtonsModal>
+                <button data-test="cancel" onClick={() => setShowDeleteModal(false)}>
+                  No, go back
+                </button>
+                <ButtonYes
+                  data-test="confirm"
+                  onClick={() => trashIconClicked(post.id)}
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <ThreeDots
+                      type="ThreeDots"
+                      color="#000000"
+                      height={20}
+                      width={50}
+                    />
+                  ) : (
+                    "Yes, delete it"
+                  )}
+                </ButtonYes>
+              </ButtonsModal>
             </DivInsideModal>
-           
+
           </Modal>
 
           <Text>
-            {reactStringReplace(
-              post.content,
-              /(?<=[\s>]|^)#(\w*[A-Za-z_]+\w*)/g,
-              (match, i) => (
-                <span onClick={() => navigate(`/hashtag/${match}`)}>
-                  #{match}
-                </span>
+            {isEditing ? (
+              <textarea
+                ref={textInputRef}
+                value={editedContent}
+                onChange={(e) => {
+                  setEditedContent(e.target.value);
+                  setChangesMade(true);
+                }}
+                onBlur={handleCancelEdit}
+                onKeyPress={handleKeyPress}
+                onKeyDown={handleKeyDown}
+                disabled={isLoading}
+              />
+            ) : (
+              reactStringReplace(
+                post.content,
+                /(?<=[\s>]|^)#(\w*[A-Za-z_]+\w*)/g,
+                (match, i) => (
+                  <span onClick={() => navigate(`/hashtag/${match}`)}>
+                    #{match}
+                  </span>
+                )
               )
             )}
           </Text>
@@ -233,8 +298,8 @@ export default function PostBox({ post }) {
                 </ContainerDetails>
                 <ContainerImage>
                   {urlMetadataInfo &&
-                  urlMetadataInfo.images &&
-                  urlMetadataInfo.images.length > 0 ? (
+                    urlMetadataInfo.images &&
+                    urlMetadataInfo.images.length > 0 ? (
                     <LinkImage src={urlMetadataInfo.images[0]} alt="metadata" />
                   ) : (
                     <LinkImage src={NoImage} alt="metadata" />
@@ -248,6 +313,12 @@ export default function PostBox({ post }) {
     </>
   );
 }
+
+const PenTrashContainer = styled.div``;
+const PenContainer = styled.img`
+  width: 23px;
+  height: 23px;
+`;
 
 const ContainerDetails = styled.div`
   width: 302px;
