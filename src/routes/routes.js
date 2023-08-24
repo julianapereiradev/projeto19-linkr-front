f78f2475a3696a1e5e18c14fe62d30754832a336
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-24 17:36:45 -0300

    Update: infinit scroll

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,7 +9,6 @@ export const pages = {
 const API_URL = 'http://localhost:5000';
 // const API_URL = `${process.env.REACT_APP_API_URL}`
 
-
 export const backendroute = {
     postSignUp: API_URL + '/signup',
     postSignIn: API_URL + '/signin',
